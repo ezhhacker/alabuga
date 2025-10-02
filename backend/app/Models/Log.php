07fd4eaa897ebfab19bb4faf6b0023e77948217b
Adam Adam <adam@r-line.ru>
2025-10-02 19:24:05 +0300

    Add Log model

---
+++ backend/app/Models/Log.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Log extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'event_type',
+        'description',
+        'data',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'data' => 'array',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
