23bafe98e047ab62e5d0ac56e2612e691800482a
Adam Adam <adam@r-line.ru>
2025-10-02 19:24:10 +0300

    Add StoreItem model

---
+++ backend/app/Models/StoreItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StoreItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category',
+        'image',
+        'is_active',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'price' => 'integer',
+            'is_active' => 'boolean',
+        ];
+    }
+}
